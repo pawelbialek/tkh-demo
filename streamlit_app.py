34ca3bb7c0ed92d7cb80de98424b939f0efe74eb
Pawel Bialek <dev@bialek.cc>
2025-11-20 23:38:59 +0100

    AAA

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,18 @@ from run_spider import run_scrapy_spider
 import time
 import os
 
+# better_run_spider.py
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+def run_spider_blocking():
+    process = CrawlerProcess(get_project_settings())
+    
+    # Replace 'tkh_all_clean' with your spider name
+    process.crawl('tkh_all_clean')
+    process.start()  # This blocks until spider finishes
+
+
 st.title("My Scrapy Spider Launcher")
 
 if st.button("Run Spider (Pure Python)"):
