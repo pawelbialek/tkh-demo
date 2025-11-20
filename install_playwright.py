32a189bf5f4e62ae04a7102d7faa28e4974664d8
Pawel Bialek <dev@bialek.cc>
2025-11-21 00:58:27 +0100

    AAA

--- install_playwright.py
+++
@@ -1,7 +0,0 @@
-# install_playwright.py
-import os
-import subprocess
-import sys
-
-print("Installing Playwright browsers...")
-subprocess.run(["playwright", "install", "--with-deps", "chromium"], check=True)
